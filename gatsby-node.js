bf300563c4e656bfe6cc4616baa76a99b74ae471
Dean Clatworthy <dean.clatworthy@gmail.com>
2019-12-26 17:41:33 +0000

    Created test case

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,58 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const report = require("gatsby-cli/lib/reporter")
+const firebase = require("firebase-admin")
 
-// You can delete this file if you're not using it
+const credential = require("./credentials.json")
+const types = [
+  {
+    type: `Item`,
+    collection: `items2`,
+    map: doc => {
+      return {
+        ...doc,
+      }
+    },
+  },
+]
+
+exports.sourceNodes = async ({ actions, createContentDigest }, {}) => {
+  try {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(credential),
+    })
+  } catch (e) {
+    report.warn(
+      "Could not initialize Firebase. Please check `credential` property in gatsby-config.js"
+    )
+    report.warn(e)
+    return
+  }
+
+  const db = firebase.firestore()
+
+  const { createNode } = actions
+
+  const promises = types.map(
+    async ({ collection, type, map = node => node }) => {
+      const snapshot = await db.collection(collection).get()
+      for (let doc of snapshot.docs) {
+        const nodeData = map(doc.data())
+        const nodeMeta = {
+          id: doc.id,
+          parent: null,
+          children: [],
+          internal: {
+            type,
+            content: JSON.stringify(nodeData),
+            contentDigest: createContentDigest(nodeData),
+          },
+        }
+        createNode(Object.assign({}, nodeData, nodeMeta))
+        Promise.resolve()
+      }
+    }
+  )
+
+  await Promise.all(promises)
+
+  return
+}
